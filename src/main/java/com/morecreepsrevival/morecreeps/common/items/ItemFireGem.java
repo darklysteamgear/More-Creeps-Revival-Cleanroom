ff48b4753adac5addf1a504c6c8dc6ac0962edbe
Renan Colombo <48870573+focamacho@users.noreply.github.com>
2025-10-08 19:32:52 -0300

    fix: item fire gem range

--- src/main/java/com/morecreepsrevival/morecreeps/common/items/ItemFireGem.java
+++ src/main/java/com/morecreepsrevival/morecreeps/common/items/ItemFireGem.java
@@ -1,9 +1,6 @@
 package com.morecreepsrevival.morecreeps.common.items;
 
-import com.morecreepsrevival.morecreeps.common.entity.EntityGuineaPig;
-import com.morecreepsrevival.morecreeps.common.entity.EntityHotdog;
-import com.morecreepsrevival.morecreeps.common.entity.EntityHunchback;
-import com.morecreepsrevival.morecreeps.common.entity.EntityPreacher;
+import com.morecreepsrevival.morecreeps.common.entity.*;
 import com.morecreepsrevival.morecreeps.common.sounds.CreepsSoundHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -13,6 +10,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
@@ -20,9 +18,7 @@ import javax.annotation.Nonnull;
 public class ItemFireGem extends CreepsItem {
     public ItemFireGem() {
         super("fire_gem");
-
         setMaxStackSize(1);
-
         setMaxDamage(64);
     }
 
@@ -30,15 +26,14 @@ public class ItemFireGem extends CreepsItem {
     @Nonnull
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, @Nonnull EnumHand hand) {
         player.playSound(CreepsSoundHandler.fireGemSound, 1.0f, 1.0f);
-
         player.getHeldItem(hand).damageItem(1, player);
-
         player.swingArm(hand);
 
-        for (Entity entity : world.getEntitiesWithinAABB(EntityLivingBase.class, player.getEntityBoundingBox().expand(10.0d, 10.0d, 10.0d))) {
-            EntityLivingBase livingEntity = (EntityLivingBase) entity;
-
-            if (livingEntity instanceof EntityHotdog || livingEntity instanceof EntityHunchback || livingEntity instanceof EntityPlayer || livingEntity instanceof EntityGuineaPig || livingEntity instanceof EntityPreacher) {
+        for (EntityLivingBase entity : world.getEntitiesWithinAABB(EntityLivingBase.class,
+                new AxisAlignedBB(player.posX - 5, player.posY - 5, player.posZ - 5,
+                                    player.posX + 5, player.posY + 5, player.posZ + 5)
+        )) {
+            if ((entity instanceof EntityCreepBaseOwnable && ((EntityCreepBaseOwnable) entity).isTamed()) || entity instanceof EntityHunchback || entity instanceof EntityPlayer || entity instanceof EntityPreacher) {
                 continue;
             }
 
@@ -46,14 +41,12 @@ public class ItemFireGem extends CreepsItem {
                 double d = itemRand.nextGaussian() * 0.02D;
                 double d1 = itemRand.nextGaussian() * 0.02D;
                 double d2 = itemRand.nextGaussian() * 0.02D;
-                world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, livingEntity.posX + (double) (itemRand.nextFloat() * 1.5F), livingEntity.posY + 0.5D + (double) (itemRand.nextFloat() * 2.5F), livingEntity.posZ + (double) (itemRand.nextFloat() * 1.5F), d, d1, d2);
+                world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, entity.posX + (double) (itemRand.nextFloat() * 1.5F), entity.posY + 0.5D + (double) (itemRand.nextFloat() * 2.5F), entity.posZ + (double) (itemRand.nextFloat() * 1.5F), d, d1, d2);
             }
 
-            livingEntity.attackEntityFrom(DamageSource.IN_FIRE, 2.0f);
-
-            livingEntity.motionY += 0.5d;
-
-            livingEntity.setFire(15);
+            entity.attackEntityFrom(DamageSource.IN_FIRE, 2.0f);
+            entity.motionY += 0.5d;
+            entity.setFire(15);
         }
 
         return super.onItemRightClick(world, player, hand);
